47d5c7e1c7d03d410d4ca283aba7d4134c7274ed
Chintan Grover <chinxgrover@gmail.com>
2023-09-24 23:28:50 +0530

    User signup done

--- src/components/LoginLogout/UserReg.jsx
+++ src/components/LoginLogout/UserReg.jsx
@@ -12,15 +12,56 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { IoIosRocket } from "react-icons/io";
 import Multiselect from "multiselect-react-dropdown";
+import { Navigate } from "react-router-dom";
 const UserReg = () => {
   const skills = ["skill1", "skill2", "skill3", "skill4", "skill5", "skill6", "skill7"]
   const interest =["Intersts1", "Interests2", "Interests3", "Interests4", "Interests5", "Interests6", "Interests7", "Interests8"]
+  const template = {
+    name : "",
+    dob : "",
+    phone : "",
+    address : "",
+    pincode : "",
+    city : "",
+    state : "",
+    aadhar : "",
+   };
   
+  const [user, setUser] = useState({ ...template });
   const [selectedSkills, setSelectedSkills ] = useState([]);
   const [selectedInterests, setSelectedInterests ] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const regUser = () => {
+    setIsLoading(true);
+    const uid = sessionStorage.getItem("uid");
+    const temp = {...user, selectedSkills : selectedSkills, selectedInterests : selectedInterests, uid : uid, userType: "user"};
+    fetch("https://sih23-backend.vercel.app/register", {
+      method: "POST",
+      body: JSON.stringify(temp),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+    .then((resposne) => resposne.json())
+    .then((data) => {
+      console.log(data);
+      if(data.status === 200) {
+        setIsLoading(!isLoading);
+        setUser(template);
+        sessionStorage.setItem("userType", "user");
+        navigate("/profile")
+      }
+      else {
+        console.log("Something went wrong");
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  } 
   return (
     <>
       <Box className="userReg" width={"100vw"} height={"100vh"}>
@@ -61,12 +102,20 @@ const UserReg = () => {
             <Stack spacing={4}>
               <HStack>
                 <Input
+                  value={user.name}
+                  onChange={(e) =>
+                    setUser({ ...user, name: e.target.value })
+                  }
                   variant={"filled"}
                   _placeholder={{ color: "black" }}
                   placeholder="Full Name"
                   size="md"
                 />
                 <Input
+                  value={user.dob}
+                  onChange={(e) =>
+                    setUser({ ...user, dob: e.target.value })
+                  }
                   variant={"filled"}
                   _placeholder={{ color: "black" }}
                   placeholder="DOB"
@@ -77,6 +126,10 @@ const UserReg = () => {
               <InputGroup>
                 <InputLeftAddon children="+91" />
                 <Input
+                  value={user.phone}
+                  onChange={(e) =>
+                    setUser({ ...user, phone: e.target.value })
+                  }
                   type="tel"
                   variant={"filled"}
                   placeholder="phone number"
@@ -84,12 +137,20 @@ const UserReg = () => {
               </InputGroup>
               <input type="file" className="file-input file-input-bordered file-input-warning w-full max-w-xs" />
               <Input
+                value={user.address}
+                onChange={(e) =>
+                  setUser({ ...user, address: e.target.value })
+                }
                 variant={"filled"}
                 _placeholder={{ color: "black" }}
                 placeholder="Enter Your Address"
                 size="md"
               />
               <Input
+                value={user.pincode}
+                  onChange={(e) =>
+                    setUser({ ...user, pincode: e.target.value })
+                  }
                 variant={"filled"}
                 _placeholder={{ color: "black" }}
                 placeholder="Address Pincode"
@@ -97,12 +158,20 @@ const UserReg = () => {
               />
               <HStack>
                 <Input
+                  value={user.city}
+                  onChange={(e) =>
+                    setUser({ ...user, city: e.target.value })
+                  }
                   variant={"filled"}
                   _placeholder={{ color: "black" }}
                   placeholder="City"
                   size="md"
                 />
                 <Input
+                  value={user.state}
+                  onChange={(e) =>
+                    setUser({ ...user, state: e.target.value })
+                  }
                   variant={"filled"}
                   _placeholder={{ color: "black" }}
                   placeholder="State"
@@ -111,21 +180,28 @@ const UserReg = () => {
               </HStack>
 
               <Input
+                value={user.aadhar}
+                  onChange={(e) =>
+                    setUser({ ...user, aadhar: e.target.value })
+                  }
                 variant={"filled"}
                 _placeholder={{ color: "black" }}
                 placeholder="Adhhar Number"
                 size="md"
               />
               <Select
+                onChange={(e) => 
+                  setUser({...user, experience : e.target.value})
+                }
                 placeholder="Select your Professional Experience"
                 variant={"filled"}
               >
-                <option value="option1">0 - 1 Years</option>
-                <option value="option2">1 - 2 Years</option>
-                <option value="option3">2 - 3 Years</option>
-                <option value="option3">3 - 4 Years</option>
-                <option value="option3">4 - 5 Years</option>
-                <option value="option3">5+ Years</option>
+                <option value="0 - 1 Years">0 - 1 Years</option>
+                <option value="1 - 2 Years">1 - 2 Years</option>
+                <option value="2 - 3 Years">2 - 3 Years</option>
+                <option value="3 - 4 Years">3 - 4 Years</option>
+                <option value="4 - 5 Years">4 - 5 Years</option>
+                <option value="5+ Years">5+ Years</option>
               </Select>
 
               <Multiselect
@@ -157,7 +233,7 @@ const UserReg = () => {
                 }}
                 options={skills}
               />
-              <Button size={"lg"} colorScheme="orange" variant={"solid"}>
+              <Button onClick = {regUser} size={"lg"} colorScheme="orange" variant={"solid"}>
                 Submit
               </Button>
             </Stack>
